6409c2dbf3ff5bd888b2a302d1553fde0e48796e
msnodeve <msnodeve@gmail.com>
2019-05-06 19:26:59 +0900

    다른 방법의 프로젝트 생성

---
+++ testPjt001/src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+    public static void main(String[] agrs){
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+
+        TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
+        transportationWalk.move();
+        ctx.close();
+    }
+}
---
+++ testPjt001/src/main/java/TransportationWalk.java
@@ -0,0 +1,5 @@
+public class TransportationWalk {
+    public void move(){
+        System.out.println("보도로 이동합니다.");
+    }
+}
