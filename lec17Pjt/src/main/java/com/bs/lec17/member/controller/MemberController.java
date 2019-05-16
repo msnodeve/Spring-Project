e4fe8e40f289ce19a5956228c97bc2a8aded76bb
msnodeve <msnodeve@gmail.com>
2019-05-17 00:00:20 +0900

    Controller 구현 1

--- lec17Pjt/src/main/java/com/bs/lec17/member/controller/MemberController.java
+++ lec17Pjt/src/main/java/com/bs/lec17/member/controller/MemberController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bs.lec17.member.Member;
 import com.bs.lec17.member.service.MemberService;
 
 @Controller
+@RequestMapping("member/")
 public class MemberController {
 
 	@Autowired
@@ -37,10 +39,11 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="/memLogin", method=RequestMethod.POST)
-	public String memLogin(Model model, HttpServletRequest request) {
+	public String memLogin(Model model, @RequestParam("memId") String memId,
+			@RequestParam("memPw") String memPw) {
 		
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
+		// String memId = request.getParameter("memId");
+		// String memPw = request.getParameter("memPw");
 		
 		Member member = service.memberSearch(memId, memPw);
 		
